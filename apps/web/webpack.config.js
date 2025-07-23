f37f8b8794866420e4b86223ee77164c1f2b795a
tohjbin2 <tohjbin2@gmail.com>
2025-07-24 02:00:49 +0900

    build: 설치 수정

--- apps/web/webpack.config.js
+++ apps/web/webpack.config.js
@@ -57,16 +57,13 @@ module.exports = env => {
       ],
     },
     resolve: {
+      alias: {
+        react: path.resolve(__dirname, './node_modules/react'), // 경로 강제 지정
+        'react-dom': path.resolve(__dirname, './node_modules/react-dom'), // 경로 강제 지정
+      },
       fallback: {
-        path: require.resolve('path-browserify'),
-        os: require.resolve('os-browserify/browser'),
-        // stream: require.resolve('stream-browserify'), // 추가
-        // buffer: require.resolve('buffer/'), // 추가
-        // process: require.resolve('process/browser'), // 추가
-        // stream: require.resolve('stream-browserify'),
-        // util: require.resolve('util/'),
-        // buffer: require.resolve('buffer/'),
-        // process: require.resolve('process/browser'),
+        // path: require.resolve('path-browserify'),
+        // os: require.resolve('os-browserify/browser'),
       },
       extensions: ['.js', '.jsx', '.ts', '.tsx'],
     },
@@ -74,7 +71,7 @@ module.exports = env => {
       new HtmlWebpackPlugin({
         template: './public/index.html',
         filename: 'index.html',
-        // favicon: './public/data/navbar-logo.png', // MEMO: 파비콘
+        // favicon: './public/data/favicon.png', // MEMO: 파비콘
       }),
       new MiniCssExtractPlugin({
         filename: './[name].css',
